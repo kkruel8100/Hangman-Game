1b48d1e9b2594d702cc6c7d3e892fe444aab8f33
kkruel8100 <kkruel.kruel1@gmail.com>
2017-08-04 18:52:55 -0700

    Moved test files.  Updated JS buttons and page displays.  Updated HTML and CSS.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,9 +12,11 @@ var guessArray = [];
 
 $(document).ready(function() {
 
-   //jQuery to select the header with "click-me" as its class.
-   // Whenever it is clicked...
-   $(".click-me").on("click", function() {
+
+
+   	//jQuery to select the header with "click-me" as its class.
+   	// Whenever it is clicked...
+   	$("#clickStart").on("click", function() {
 
       // ... we trigger an alert.
       alert("I've been clicked!");
@@ -22,8 +24,17 @@ $(document).ready(function() {
 		// Randomize computer choice for hidden word
 		var computerChoice = toppings[Math.floor((Math.random() * (toppings.length)))];
 		console.log("Computer chose " + computerChoice);
+
 		//Replace any character (.) in computerChoice with _
 		var replaceChoice = computerChoice.replace(/./g, "_");  
+
+		//Update HTML page
+		
+		document.getElementById("clickStart").style.visibility = "collapse";
+		document.getElementById("currentWord").innerHTML = "Pizza Topping";
+		document.getElementById("secretWord").innerHTML = replaceChoice;
+		document.getElementById("letGuess").innerHTML = "Letters Guessed";
+		document.getElementById("countDown").innerHTML = "Wrong Guesses remaining: " + wrgGuess;
 
  		//Capture keystroke from user and converts to lower case
 		document.onkeyup = function (event) {
@@ -41,6 +52,8 @@ $(document).ready(function() {
 				else {
 					//push to guess array	
 					guessArray.push(userguess);
+					document.getElementById("letGuess").innerHTML = "Letters Guessed";
+					document.getElementById("array").innerHTML = guessArray;
 					console.log(guessArray);
 		
 					//test for userguess in computerChoice - result is true or false
@@ -66,17 +79,28 @@ $(document).ready(function() {
 						//For loop to run the replaceAt for all charactersi
 						for (i=0; i<locations.length; i++) {
 							replaceChoice = replaceChoice.replaceAt(locations[i],userguess);
+							document.getElementById("secretWord").innerHTML = replaceChoice;
 							console.log(replaceChoice);
 						}
+
+						
+
 
 						//All letters found	
 						if (replaceChoice===computerChoice) {
 							alert("Congratulations! You win to live another day!");
 							wins++;
 							console.log(wins);
+							guessArray = [];
+							wrgGuess = 6;
+							console.log(guessArray);
 							document.onkeyup = function(event) {
 								return false;
 							}
+							document.getElementById("array").innerHTML = guessArray;	
+							document.getElementById("wins").innerHTML = "Wins: " + wins;
+							document.getElementById("clickStart").style.visibility = "visible";
+
 						}
 
 					}//end of if resultCC===true
@@ -85,17 +109,43 @@ $(document).ready(function() {
 					else {
 						wrgGuess--;
 						console.log(wrgGuess);
+						document.getElementById("countDown").innerHTML = "Wrong Guesses remaining: " + wrgGuess;
 						if (wrgGuess===0) {
 							alert("You're Dead!  Hope you are a cat or you believe in Karma.");
 							losses++;
+							guessArray = [];
+							wrgGuess = 6;
 							document.onkeyup = function(event) {
 								return false;
 							}
+							document.getElementById("array").innerHTML = guessArray;
+							document.getElementById("losses").innerHTML = "Losses: " + losses;
+							document.getElementById("clickStart").style.visibility = "visible";
 						}
 					}
 				} //end of else
 		};//onkeyup
+		
+	
 	});//click-me game
+
+	//jQuery to create reset button
+	$("#reset").on("click", function () {
+		var wins=0;
+		var losses=0;
+		var wrgGuess=6;
+		var guessArray = [];
+		alert("You clicked reset");
+		console.log("losses " + losses);
+		console.log("wins " + wins);
+		console.log("wrgGuess" + wrgGuess);
+		console.log("guessArray" + guessArray);
+		document.getElementById("wins").innerHTML = "Wins: ";
+		document.getElementById("losses").innerHTML = "Losses: ";
+		document.getElementById("countDown").innerHTML = "Wrong Guesses remaining: " + wrgGuess;
+		document.getElementById("array").innerHTML = guessArray;
+	});
+
 });//document ready
 
 
